5e8755edbb570fed068ddfe192244af5c232636d
ENM185 <ericnathanmiller@icloud.com>
2020-06-26 22:17:30 -0400

    Better hill climbing and code organization

--- main.py
+++ main.py
@@ -27,34 +27,32 @@ def _concat_weights(f1, f2, labels, k=20):
     val_size = 47500
     results = []
 
-    #start with 1:1 ratio
+    #start with 1:1 ratio (.5:.5 really)
     classifier = NearestNeighbors()
-    train_data = np.concatenate((f1[:val_size], f2[:val_size]), axis=1)
+    train_data = np.concatenate((.5*f1[:val_size], .5*f2[:val_size]), axis=1)
     train_labels = labels[:val_size]
-    test_data = np.concatenate((f1[val_size:], f2[val_size:]), axis=1)
+    test_data = np.concatenate((.5*f1[val_size:], .5*f2[val_size:]), axis=1)
     test_labels = labels[val_size:]
     classifier.train(train_data, train_labels)
     prev_accuracy = classifier.test(test_data, test_labels, k=k)
-    m = 1.0
-    print((prev_accuracy, m))
+    m = .5
+    print((m, prev_accuracy))
 
     #try different 1:m ratios
-    lower, upper = None, None
-    for _ in range(10):
+    lower, upper = 0, 1
+    for _ in range(5):
         new_accuracies = [] # values of f(successor)
-        next_m = [.75*m, 1.5*m] #default if either lower/upper don't exist
-        if lower is not None:
-            next_m[0] = (lower + m) / 2
-        if upper is not None:
-            next_m[1] = (upper + m) / 2
+        next_m = [(lower + m) / 2, (upper + m) / 2]
+
         for x in next_m:
-            train_data = np.concatenate((f1[:val_size], x*f2[:val_size]), axis=1)
+            train_data = np.concatenate(((1-x)*f1[:val_size], x*f2[:val_size]), axis=1)
             train_labels = labels[:val_size]
-            test_data = np.concatenate((f1[val_size:], x*f2[val_size:]), axis=1)
+            test_data = np.concatenate(((1-x)*f1[val_size:], x*f2[val_size:]), axis=1)
             test_labels = labels[val_size:]
             classifier.train(train_data, train_labels)
             new_accuracies.append(classifier.test(test_data, test_labels, k=k))
         if max(new_accuracies) < prev_accuracy:
+            print("Stopping because next highest accuracy is {}".format(max(new_accuracies)))
             break
         elif max(new_accuracies) == new_accuracies[0]:
             upper = m
@@ -64,9 +62,9 @@ def _concat_weights(f1, f2, labels, k=20):
             lower = m
             m = next_m[1]
             prev_accuracy = new_accuracies[1]
-        print((prev_accuracy, m))
+        print((m, prev_accuracy))
 
-    return (prev_accuracy, m) #not necessarily the last one, but usually is
+    return (prev_accuracy, m/(1-m)) #we want to keep multiplier at 1 for the first feature
 
 #features are the arrays of extracted features
 def _concat_many_weights(labels, features):
@@ -82,10 +80,11 @@ def _concat_many_weights(labels, features):
 
 #features are pointers to the functions
 def concat(train_images, train_labels, test_images, test_labels, *features):
+    print("Concatenating features...")
     train_extracted = []
     norms = []
     for f in features:
-        train_extracted.append(f(train_images))
+        train_extracted.append(f(train_images).astype(float))
         norms.append(np.linalg.norm(train_extracted[-1], ord='fro'))
         train_extracted[-1] /= norms[-1]
     mult = _concat_many_weights(train_labels, train_extracted)[1]
@@ -102,6 +101,35 @@ def concat(train_images, train_labels, test_images, test_labels, *features):
     print("Accuracy on test data:")
     print(classifier.test(test_data, test_labels, k=25))
 
+def train_and_test(train_images, train_labels, test_images, test_labels, *features):
+    if len(features) > 1:
+        return concat(train_images, train_labels, test_images, test_labels, *features)
+    
+    #apply filters and choose how many tests to try
+    train_images = features[0](train_data['images'])
+    train_labels = train_data['labels']
+
+    test_images = features[0](test_data['images'])
+    test_labels = test_data['labels']
+
+    #train and test model
+    print("Training and testing model")
+    classifier = NearestNeighbors()
+    start = time.process_time()
+    classifier.train(train_images, train_labels)
+    classifier.test(test_images, test_labels, k=1)
+    end = time.process_time()
+    print("Training and testing took {} seconds".format(end - start))
+
+    #retest for different k's
+    #doesn't work with scikit
+    results = []
+    for k in range(1,30):
+        results.append([k, classifier.test(test_images, test_labels, k=k)])
+    for row in results:
+        print("({},{})".format(row[0],row[1]))
+    
+
 #load data
 train_data = loader.load_training_data("cifar/data")
 test_data = loader.load_test_data("cifar/data")
@@ -110,42 +138,4 @@ test_data = loader.load_test_data("cifar/data")
 train_data['images'] = np.array(train_data['images']) / float(255)
 test_data['images'] = np.array(test_data['images']) / float(255)
 
-concat(train_data['images'], train_data['labels'], test_data['images'], test_data['labels'], fe.pixel_histogram, fe.hog)
-
-'''
-#apply filters and choose how many tests to try
-train_grayscale = fe.grayscale(train_data['images'])
-norm1 = np.linalg.norm(train_grayscale, ord='fro')
-train_grayscale /= norm1
-train_hog = fe.hog(train_data['images'])
-norm2 = np.linalg.norm(train_hog, ord='fro')
-train_hog /= norm2
-
-train_images = np.concatenate((train_grayscale, train_hog), axis=1)
-train_labels = train_data['labels']
-
-test_grayscale = fe.grayscale(test_data['images'])
-test_grayscale /= norm1
-test_hog = fe.hog(test_data['images'])
-test_hog /= norm2
-
-test_images = np.concatenate((test_grayscale, test_hog), axis=1)
-test_labels = test_data['labels']
-
-#train and test model
-print("Training and testing model")
-classifier = NearestNeighbors()
-start = time.process_time()
-classifier.train(train_images, train_labels)
-classifier.test(test_images, test_labels, k=1)
-end = time.process_time()
-print("Training and testing took {} seconds".format(end - start))
-
-#retest for different k's
-#doesn't work with scikit
-results = []
-for k in range(1,30):
-    results.append([k, classifier.test(test_images, test_labels, k=k)])
-for row in results:
-    print("({},{})".format(row[0],row[1]))
-'''
+train_and_test(train_data['images'], train_data['labels'], test_data['images'], test_data['labels'], fe.grayscale)
